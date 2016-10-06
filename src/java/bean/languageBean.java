069d9c85ae2e1f09c7cfae94d2b423b849f7f3f5
mmejia0422 <mangelmejia@msn.com>
2016-10-05 23:03:27 -0600

    Historia: https://trello.com/c/4q1i9gTn Se completo la tarea: Implementar la internacionalización para los idiomas de ingles y español Comentarios: Falta cambiar los outputlabel de los mantenimientos

--- src/java/bean/languageBean.java
+++ src/java/bean/languageBean.java
@@ -5,8 +5,15 @@
  */
 package bean;
 
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
 
 /**
  *
@@ -14,9 +21,48 @@ import javax.faces.bean.SessionScoped;
  */
 @ManagedBean(name="languageBean")
 @SessionScoped
-public class languageBean {
+public class languageBean implements Serializable{
 
-    public languageBean() {
-    }
-    
+	private static final long serialVersionUID = 1L;
+
+	private String localeCode;
+
+	private static Map<String,Object> countries;
+	static{
+		countries = new LinkedHashMap<String,Object>();
+                Locale espanol=new Locale("es_ES");
+		countries.put("English", Locale.ENGLISH); //label, value
+		countries.put("Español", Locale.SIMPLIFIED_CHINESE);
+	}
+
+	public Map<String, Object> getCountriesInMap() {
+		return countries;
+	}
+
+
+	public String getLocaleCode() {
+		return localeCode;
+	}
+
+
+	public void setLocaleCode(String localeCode) {
+		this.localeCode = localeCode;
+	}
+
+	//value change event listener
+	public void countryLocaleCodeChanged(ValueChangeEvent e){
+
+		String newLocaleValue = e.getNewValue().toString();
+
+		//loop country map to compare the locale code
+                for (Map.Entry<String, Object> entry : countries.entrySet()) {
+
+        	   if(entry.getValue().toString().equals(newLocaleValue)){
+
+        		FacesContext.getCurrentInstance()
+        			.getViewRoot().setLocale((Locale)entry.getValue());
+
+        	  }
+               }
+	}
 }
